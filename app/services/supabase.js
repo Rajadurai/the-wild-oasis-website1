3e3ede238fbd0d807b10ebbd663ed4b43ab0975f
rajadurai <rajadurai1981@gmail.com>
2026-06-26 13:56:19 +0530

    setting supabase db

--- app/services/supabase.js
+++ app/services/supabase.js
@@ -1,5 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://cvvykqfxtbbtmelgyjrc.supabase.co";
-const supabaseKey = "sb_publishable_h_x-AW7RnT8QuQNfDi6--w_ZxxA-yUC";
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY,
+);
 export default supabase;
